c75f34a7812275a4db6ac6b2d68d0d6bad776755
meovncute <meovncute123@gmail.com>
2025-11-14 15:28:47 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,26 @@
-local Main = require(getcustomasset("MainModule1.rbxm"))
+local Loader = {}
 
-Main.Play("https://github.com/meovncute/Jj/raw/refs/heads/main/MainModule1.rbxm")
+local url = "https://github.com/meovncute/Jj/raw/refs/heads/main/MainModule2.lua"
+
+function Loader.Load()
+    local success, code = pcall(function()
+        return game:HttpGet(url)
+    end)
+
+    if not success then
+        warn("[LoaderModule] Không thể tải file:", code)
+        return nil
+    end
+
+    local func, err = loadstring(code)
+    if not func then
+        warn("[LoaderModule] Lỗi loadstring:", err)
+        return nil
+    end
+
+    local moduleReturned = func()
+
+    return moduleReturned -- trả về table/module
+end
+
+return Loader
